9c81a851a4c201f0905ecb2243260341dc1d7a5a
titusjerold18 <jos.tjerold18@gmail.com>
2024-09-05 23:09:06 +0530

    project upload

---
+++ landing/new.js
@@ -0,0 +1,13 @@
+c1=document.getElementById("c1")
+ind1=document.getElementById("ind1")
+function def2()
+{
+    ind1.style.display="none"
+    top3.style.display="block"
+}
+top3=document.getElementById("top3")
+function def()
+{
+    top3.style.display="none"
+    ind1.style.display="block"
+}
---
+++ project1/hello.js
@@ -0,0 +1,116 @@
+n7=document.getElementById("n7")
+n8=document.getElementById("n8")
+n9=document.getElementById("n9")
+n4=document.getElementById("n4")
+n5=document.getElementById("n5")
+n6=document.getElementById("n6")
+n1=document.getElementById("n1")
+n2=document.getElementById("n2")
+n3=document.getElementById("n3")
+head1=document.getElementById("head1")
+nn=document.getElementById("nn")
+nm=document.getElementById("nm")
+var m=0
+function d7()
+{
+    head1.append(n7.textContent)
+    var n=m+n7.textContent
+    m=Number(n)
+}
+function d8()
+{
+    head1.append(n8.textContent)
+    var n=m+n8.textContent
+    m=Number(n)
+    
+}
+function d9()
+{
+    head1.append(n9.textContent)
+    var n=m+n9.textContent
+    m=Number(n)
+}
+function d4()
+{
+    head1.append(n4.textContent)
+    var n=m+n4.textContent
+    m=Number(n)
+}
+function d5()
+{
+    head1.append(n5.textContent)
+    var n=m+n5.textContent
+    m=Number(n)
+}
+function d6()
+{
+    head1.append(n6.textContent)
+    var n=m+n6.textContent
+    m=Number(n)
+}
+function d1()
+{
+    head1.append(n1.textContent)
+    var n=m+n1.textContent
+    m=Number(n)
+}
+function d2()
+{
+    head1.append(n2.textContent)
+    var n=m+n2.textContent
+    m=Number(n)
+}
+function d3()
+{
+    head1.append(n3.textContent)
+    var n=m+n3.textContent
+    m=Number(n)
+}
+function d0()
+{
+    head1.append(n0.textContent)
+    var n=m+n0.textContent
+    m=Number(n)
+}
+var num=0
+function dd()
+{
+    head1.append(nn.textContent)
+    
+   
+}
+function dd1()
+{
+    head1.textContent=""
+   
+}
+function de()
+{
+    head1.append(mm.textContent)
+}
+
+function ddd()
+{
+    head1.append(nm.textContent)
+    
+}
+function dddd()
+{
+    try{
+    head1.textContent=eval(head1.textContent)
+    }
+    catch(error)
+    {
+        head1.textContent="Error" ;
+    }
+}
+function de1()
+{
+    head1.append(mm1.textContent)
+}
+function de2()
+{
+    head1.append(mm2.textContent)
+}
+
+
